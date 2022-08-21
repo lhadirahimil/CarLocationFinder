b0542e0cf4d8fe8e611ea1f97f6729649397af7f
hadi Rahimi | هادی رحیمی <76452700+lhadirahimil@users.noreply.github.com>
2022-08-21 03:13:09 -0700

    Update Constants.kt

--- app/src/main/java/com/hadirahimi/locationFinder/Utils/Constants.kt
+++ app/src/main/java/com/hadirahimi/locationFinder/Utils/Constants.kt
@@ -3,8 +3,8 @@ package com.hadirahimi.locationFinder.Utils
 object Constants
 {
     const val REQUEST_GPS_CODE = 100
-    const val API_KEY = "service.ggYMERA1tekhVGgxkLM5LvkoOeJq2dhDBdEyIDVJ"
-    //service.ggYMERA1tekhVGgxkLM5LvkoOeJq2dhDBdEyIDVJ
+    const val API_KEY = "not found"
+  
     const val CAR_LOCATION : String = "car_location"
     const val CAR_DATABASE : String = "car_database"
     const val BASE_URL : String = "https://api.neshan.org/v2/"
